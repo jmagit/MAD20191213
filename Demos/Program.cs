da254c3702dcd34bf01458992f9415d7c56139c1
Javier <jmartin@grupoloyal.com>
2019-12-15 18:53:41 +0100

    Inicio

---
+++ Demos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demos {
+    class Program: IDisposable {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args) {
+         }
+
+        public void Dispose() {
+            throw new NotImplementedException();
+        }
+    }
+}
